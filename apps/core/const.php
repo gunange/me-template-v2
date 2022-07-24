e030802e3d73a4d6ec45690a06545279f12b2f2a
gunange <gunangeh7@gmail.com>
2022-07-25 00:16:06 +0900

    Update Core

--- apps/core/const.php
+++ apps/core/const.php
@@ -2,7 +2,8 @@
 define('BASEURL', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' 
 	? "https" 
 	: "http") 
-	. "://$_SERVER[HTTP_HOST]" . dirname($_SERVER['SCRIPT_NAME']) . (!str_ends_with(dirname($_SERVER['SCRIPT_NAME']), "/") ? "/" : "" )  );
+	. "://$_SERVER[HTTP_HOST]" . dirname($_SERVER['SCRIPT_NAME']) 
+	. (!str_ends_with(dirname($_SERVER['SCRIPT_NAME']), "/") ? "/" : "" )  );
 define('BaseAssets', BASEURL . 'public/assets/');
 
 
