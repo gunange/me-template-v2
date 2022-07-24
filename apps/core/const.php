d159ee379b412010d10912e6ed6cd6b47cddaaa7
gunange <gunangeh7@gmail.com>
2022-07-25 00:10:24 +0900

    Perubahan Const Pada Core

--- apps/core/const.php
+++ apps/core/const.php
@@ -1,5 +1,8 @@
 <?php
-define('BASEURL', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]" . dirname($_SERVER['SCRIPT_NAME']));
+define('BASEURL', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' 
+	? "https" 
+	: "http") 
+	. "://$_SERVER[HTTP_HOST]" . dirname($_SERVER['SCRIPT_NAME']) . (!str_ends_with(dirname($_SERVER['SCRIPT_NAME']), "/") ? "/" : "" )  );
 define('BaseAssets', BASEURL . 'public/assets/');
 
 
